4de67ad24fb2351c1506c3b758bc07a4bbde1137
pgm-coliwill <coliwill@student.arteveldehs.be>
2025-02-27 16:08:22 +0100

    form for parents done

--- kampvuur/src/components/ui/forms/RegisterOuderForms.tsx
+++ kampvuur/src/components/ui/forms/RegisterOuderForms.tsx
@@ -21,12 +21,12 @@ export default function RegisterOuderForms() {
 
             <div className={styles.flexForm}>
               <div>
-                <label htmlFor="voornaam">Voornaam</label>
-                <input type="text" id="voornaam" />
+                <label htmlFor="name">Voornaam</label>
+                <input type="text" id="name" />
               </div>
               <div>
-                <label htmlFor="familienaam">Familienaam</label>
-                <input type="text" id="familienaam" />
+                <label htmlFor="familyname">Familienaam</label>
+                <input type="text" id="familyname" />
               </div>
             </div>
 
@@ -40,7 +40,36 @@ export default function RegisterOuderForms() {
 
         {step === 2 && (
           <div className={styles.secondForm}>
-            <div>Groups</div>
+            <div className={styles.flexForm}>
+              <div>
+                <label htmlFor="name-child">Naam kind</label>
+                <input type="text" id="name-child" />
+              </div>
+              <div>
+                <label htmlFor="familyname-child">Familienaam kind</label>
+                <input type="text" id="familyname-child" />
+              </div>
+            </div>
+
+            <div className={styles.flexform}>
+              <div>
+                <label htmlFor="relation-with-child">Relatie met kind</label>
+                <select name="relation-with-child" id="relation-with-child">
+                  <option value="mother">Moeder</option>
+                  <option value="father">Vader</option>
+                  <option value="guardian">Voogd</option>
+                </select>
+              </div>
+
+              <div>
+                <label htmlFor="group">Groep</label>
+                <select name="group" id="group">
+                  <option value="baby">Baby</option>
+                  <option value="peuter">Peuter</option>
+                  <option value="kleuter">Kleuter</option>
+                </select>
+              </div>
+            </div>
           </div>
         )}
       </form>
