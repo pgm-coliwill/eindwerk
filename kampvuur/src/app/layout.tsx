4b71edeb6aebd01d62b108948bc5ff4950d89ae3
pgm-coliwill <coliwill@student.arteveldehs.be>
2025-02-27 17:05:33 +0100

    aws init

--- kampvuur/src/app/layout.tsx
+++ kampvuur/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import QueryProvider from "@/context/QueryProvider";
 import { montserrat, poppins } from "@/styles/fonts";
 import "@styles/global.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "@/aws-exports";
+
+// Initialize AWS Amplify
+Amplify.configure(awsExports);
 
 export default function RootLayout({
   children,
@@ -10,9 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.variable} ${poppins.variable}`}>
-        <QueryProvider>
-          {children} 
-        </QueryProvider>
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
